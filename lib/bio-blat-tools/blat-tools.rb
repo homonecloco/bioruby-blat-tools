7b7546168547795f0505070a283734d725401c81
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2017-09-14 19:02:02 +0100

    Added rep_match to the covered function, to be consistent with the millisBand method

--- lib/bio-blat-tools/blat-tools.rb
+++ lib/bio-blat-tools/blat-tools.rb
@@ -12,46 +12,46 @@ module Bio
 						yield Hit.new(line)
 					else
 
-                                 if /^\d/ =~ line 
-                                      flag = true
-                                      redo
-                                end
-                                line = line.chomp
-                                if /\A\-+\s*\z/ =~ line
-                                      flag = true
-                                else
-                                      head << line
-                                end
-                          end
-                    end
-                  end
-
-                  def self.each_best_hit(text = '')
-                   best_aln = Hash.new
-                   self.each_hit(text) do |hit|
-                    current_matches = hit.match 
-                    current_name = hit.query_id
-                    current_identity = hit.percent_identity
-                    current_score = hit.score
-                    best_aln[current_name] = hit if best_aln[current_name] == nil or current_score > best_aln[current_name] .score
-                  end
-                  best_aln.each_value { |val| yield  val }
-                  end
-            end
+           if /^\d/ =~ line 
+            flag = true
+            redo
+          end
+          line = line.chomp
+          if /\A\-+\s*\z/ =~ line
+            flag = true
+          else
+            head << line
+          end
+        end
       end
+    end
+
+    def self.each_best_hit(text = '')
+     best_aln = Hash.new
+     self.each_hit(text) do |hit|
+      current_matches = hit.match 
+      current_name = hit.query_id
+      current_identity = hit.percent_identity
+      current_score = hit.score
+      best_aln[current_name] = hit if best_aln[current_name] == nil or current_score > best_aln[current_name] .score
+    end
+    best_aln.each_value { |val| yield  val }
+  end
+end
+end
 end
 
 class Bio::Blat::Report::Hit
-      def covered
-             match + mismatch
-      end
+  def covered
+    match + mismatch + rep_match
+  end
 
-	def query_percentage_covered
-		covered * 100.0 / query_len.to_f
-	end
+  def query_percentage_covered
+    covered * 100.0 / query_len.to_f
+  end
 
-      def target_percentage_covered
-            covered * 100.0 / target_len.to_f
-      end
+  def target_percentage_covered
+    covered * 100.0 / target_len.to_f
+  end
 
 end
